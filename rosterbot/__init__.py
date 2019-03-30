579fb3b5b100cabd12a3b3cdd38e63592b181328
Rhys Stever <38188371+rhysstever@users.noreply.github.com>
2019-03-30 13:24:02 -0400

    Verify that requests are coming from Slack

--- rosterbot/__init__.py
+++ rosterbot/__init__.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import abort, Flask, request
+from os import environ
 
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def hi():
+    if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
+        abort(403)
     return 'Hi'
