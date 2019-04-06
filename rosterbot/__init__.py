747e042f35f261f775992b458df094daae0eed4c
Rhys Stever <38188371+rhysstever@users.noreply.github.com>
2019-04-06 16:12:28 -0400

    Added commands for both addplayer and add event

--- rosterbot/__init__.py
+++ rosterbot/__init__.py
@@ -9,3 +9,14 @@ def create_roster():
         abort(403)
     return 'A roster has been created'
 
+@app.route('/', methods = ['GET', 'POST'])
+def addplayer():
+    if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
+        abort(403)
+    return 'A '
+
+@app.route('/', methods = ['GET', 'POST'])
+def addevent():
+    if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
+        abort(403)
+    return 'An event has been added'
