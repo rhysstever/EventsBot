35b5f22570f741ecab122dbb76d38e5efeedf65f
Rhys Stever <38188371+rhysstever@users.noreply.github.com>
2019-04-06 14:11:57 -0400

    removed test command

--- rosterbot/__init__.py
+++ rosterbot/__init__.py
@@ -4,12 +4,6 @@ from os import environ
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
-def hi():
-    if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
-        abort(403)
-    return 'Hi'
-
-@app.route('/', methods = ['GET', 'POST'])
 def createRoster():
     if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
         abort(403)
