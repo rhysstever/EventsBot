5595e0a8132f90b7d82ae9c1c8a42c47923e4c66
Rhys <rhys@stever.com>
2019-04-14 01:55:57 -0400

    Added if statements for this and next week

--- rosterbot/__init__.py
+++ rosterbot/__init__.py
@@ -8,7 +8,13 @@ def events():
     if request.form.get('token') != environ['SLACK_VERIFICATION_TOKEN']:
         abort(403)
 
-    if request.form.get('text') == 'today':
+    if request.form.get('text').lower() == 'today':
         return 'The events happening today'
+
+    if request.form.get('text').lower() == 'this week':
+        return 'The events happening this week'
+
+    if request.form.get('text').lower() == 'next week':
+        return 'The events happening next week'
 
     return 'bet'
